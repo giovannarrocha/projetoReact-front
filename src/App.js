b6ea1b5237b11f14dd30185ae95f4bc979e1f151
Vinicius Santos <vinioliv.santos@outlook.com>
2020-09-24 17:08:10 -0300

    Componente de busca

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import MainApp from './layout/main';
 import Mapa from './components/mapa/Mapa'
 import Display from './components/display/Display';
+import Search from './components/search/Search';
 
 function App() {
+  const [enderecoDestino, setEnderecoDestino] = useState("");
+
+  const procurarEndereco = (endereco) => {
+    setEnderecoDestino(endereco);
+  }
   return (
     <MainApp>
       <Mapa/>
-      <Display enderecoDestino="" />
+      <Search procuraEnderecoDestino={procurarEndereco} />
+      <Display enderecoDestino={enderecoDestino} />
     </MainApp>
   );
   }
---
+++ src/components/search/Search.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import SearchIcon from "@material-ui/icons/Search";
+import Input from '@material-ui/core/Input';
+import "./Search.css";
+
+interface SearchProps {
+  procuraEnderecoDestino: Function;
+}
+
+const Search: React.FC<SearchProps> = ({ procuraEnderecoDestino }) => {
+  const [enderecoProcurado, setEnderecoProcurado] = useState("");
+
+  return (
+    <div className="__search-form">
+        <Input
+            className="__search-input"
+            placeholder="Endereço"
+            value={enderecoProcurado} 
+            onChange={(e) => setEnderecoProcurado(e.target.value)}
+        />
+        <Button 
+          className="__search-button"   
+          onClick={(e) => {procuraEnderecoDestino(enderecoProcurado)}}
+        >
+          <SearchIcon className="__search-button-icon"/>
+        </Button>
+    </div>
+  );
+};
+
+export default Search;
